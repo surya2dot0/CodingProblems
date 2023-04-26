a8010edd1a1883c5bae08d516e837066ca633195
surya2dot0 <suryap1802@gmail.com>
2023-04-26 15:38:00 +0530

    Sum of Two Numbers Code

---
+++ CodingProblems/src/in/learning/SumOfTwoNumbers.java
@@ -0,0 +1,12 @@
+package in.learning;
+public class SumOfTwoNumbers {
+
+	public static int sum(int a, int b) {
+		return a+b;
+	}
+	public static void main(String[] args) {
+		int sum = sum(10,20);
+	System.out.println("The sum of given numbers = "+sum);
+	}
+	
+}
