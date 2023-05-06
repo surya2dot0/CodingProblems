a2cf17702a7d653d5cba6b495363e3db81a7c1ce
surya2dot0 <suryap1802@gmail.com>
2023-05-06 16:24:17 +0530

    Updated and Corrected

--- CodingProblems/src/in/learning/integers/ReverseInteger.java
+++ CodingProblems/src/in/learning/integers/ReverseInteger.java
@@ -6,32 +6,15 @@ public class ReverseInteger {
 	
 
 	public static void reverseInteger(int n) {
-		
-		int count = 0;
-		int num = n;
+
+		int rev = 0;
 		int reminder = 0;
-		int revNum = 0;
-		
-		do {
-			
-			num = num /10;
-			count ++;
-		}while(num>0);
-		System.out.println("Value of count"+ count);
-		int oCount = count;
-        for(int i=0; i<count;i++) {
-        	reminder = n % 10;
-        	n = n / 10;
-        if(oCount >= 1) {
-        	do {
-        	reminder = reminder * 10;
-        	oCount --;
-        	}while(oCount > 1);
-        	
-        }
-        	revNum = revNum + reminder;
-        }
-		System.out.println("Reverse Number "+ revNum);
+				while(n > 0) {
+					reminder = n % 10;
+					rev = (rev * 10) + reminder;
+					n = n /10;
+				}
+		System.out.println("Reversed Integer = " + rev);
 	}
 	public static void main(String[] arg) {
 		Scanner sc= new Scanner(System.in); 
