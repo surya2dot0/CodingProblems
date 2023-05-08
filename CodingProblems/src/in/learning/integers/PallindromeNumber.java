5e0ea1855729af0ecf50c6104ce62bdae164d390
surya2dot0 <suryap1802@gmail.com>
2023-05-08 09:53:12 +0530

    Pallindrome Basic

---
+++ CodingProblems/src/in/learning/integers/PallindromeNumber.java
@@ -0,0 +1,29 @@
+package in.learning.integers;
+
+import java.util.Scanner;
+
+public class PallindromeNumber {
+
+	public static void pallindromeNumber(int n) {
+        int chk = n;
+		int rev = 0;
+		int reminder = 0;
+		while(n > 0) {
+			reminder = n % 10;
+			rev = (rev * 10) + reminder;
+			n = n / 10;
+		}
+		if(chk == rev)
+			System.out.println("Pallindrome");
+		else System.out.println("Not a Pallindrome");
+		
+	}
+	public static void main(String[] args) {
+
+		Scanner sc= new Scanner(System.in); 
+		System.out.print("Enter a number: ");
+		int n= Integer.parseInt(sc.nextLine());
+		pallindromeNumber(n);
+	}
+
+}
