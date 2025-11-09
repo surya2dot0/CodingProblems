dc35fa80ae0a7854bc596e105c279c76854df867
Surya <suryap1802@gmail.com>
2025-11-09 10:26:24 +0530

    Added comments

--- CodingProblems/src/in/learning/integers/PallindromeNumber.java
+++ CodingProblems/src/in/learning/integers/PallindromeNumber.java
@@ -20,6 +20,7 @@ public class PallindromeNumber {
 	}
 	public static void main(String[] args) {
 
+		/* Comment added */
 		Scanner sc= new Scanner(System.in); 
 		System.out.print("Enter a number: ");
 		int n= Integer.parseInt(sc.nextLine());
